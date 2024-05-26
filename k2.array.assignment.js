6737b8585d9de2e3d975f2a5a6e035ea9d5afc52
Arun1only1 <arun1nly1@gmail.com>
2024-05-26 15:30:59 +0545

    assignment

--- k2.array.assignment.js
+++ k2.array.assignment.js
@@ -17,6 +17,29 @@ const washingMachineList = [
 
 // ? find machines whose price is greater than 900
 
+// const newList = washingMachineList.filter((item, index, array) => {
+//   return item.price > 900;
+// });
+
+// console.log(newList);
 // ? find machine whose brand is Whirlpool
+// const requiredMachine = washingMachineList.find((item, index, array) => {
+//   return item.brand === 'Whirlpool';
+// });
+
+// console.log(requiredMachine);
 
 // ? decrease price of Samsung machine by 15% as festival discount
+
+const newList = washingMachineList.map((item, index, self) => {
+  if (item.brand === 'Samsung') {
+    return {
+      brand: item.brand,
+      price: item.price - (15 / 100) * item.price,
+    };
+  }
+
+  return item;
+});
+
+console.log(newList);
